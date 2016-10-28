3122820a2ef30a828c02e65f3fb79c132f241386
Kanika Mathur <mathurkanika1@gmail.com>
2016-10-27 19:36:40 -0700

    String

---
+++ String/StringSplitAndJoin.py
@@ -0,0 +1,14 @@
+'''
+You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+
+Input Format 
+The first line contains a string consisting of space separated words.
+
+Output Format 
+Print the formatted string as explained above.
+'''
+
+
+n = raw_input()
+str1 = '-'.join(n)
+print str1
